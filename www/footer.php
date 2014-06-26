b5c9f987a590a8512e49a78d96ac269295e9c6bd
Morgy93 <morgy@ewetel.net>
2014-06-26 19:21:28 +0200

    Test

--- www/footer.php
+++ www/footer.php
@@ -1,28 +1,28 @@
-    </div>
-    <!-- jQuery (wird für Bootstrap JavaScript-Plugins benötigt) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Binde alle kompilierten Plugins zusammen ein (wie hier unten) oder such dir einzelne Dateien nach Bedarf aus -->
-    <script src="js/bootstrap.min.js"></script>
+        </div>
+        <!-- jQuery (wird für Bootstrap JavaScript-Plugins benötigt) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- Binde alle kompilierten Plugins zusammen ein (wie hier unten) oder such dir einzelne Dateien nach Bedarf aus -->
+        <script src="js/bootstrap.min.js"></script>
 
-    <script>
-    $(document).ready(function(){
+        <script>
+        $(function(){
  
-        $('.nav-tabs a').click(function (e) {
-            e.preventDefault()
-            $(this).tab('show')
-        })
-        $('.toggle-popover').popover();
+            $('.nav-tabs a').click(function (e) {
+                e.preventDefault()
+                $(this).tab('show')
+            })
+            $('.toggle-popover').popover();
         
-    });
-        
-    $('.save-button').click(function () {
-        var btn = $(this)
-        btn.button('loading')
-        $.ajax(...).always(function () {
-            btn.button('reset')
         });
-    });
-    </script>
+        
+        $('.save-button').click(function () {
+            var btn = $(this)
+            btn.button('loading')
+            $.ajax(...).always(function () {
+                btn.button('reset')
+            });
+        });
+        </script>
 
-</body>
+    </body>
 </html>
--- www/header.php
+++ www/header.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap-Basis-Vorlage</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap-Basis-Vorlage</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
     
-    <link href="css/theme.css" rel="stylesheet">
+        <link href="css/theme.css" rel="stylesheet">
 
-    <!-- Unterstützung für Media Queries und HTML5-Elemente im Internet Explorer über HTML5 shim und Respond.js -->
-    <!-- ACHTUNG: Respond.js funktioniert nicht, wenn du die Seite über file:// aufrufst -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-    <div class="container">
+        <!-- Unterstützung für Media Queries und HTML5-Elemente im Internet Explorer über HTML5 shim und Respond.js -->
+        <!-- ACHTUNG: Respond.js funktioniert nicht, wenn du die Seite über file:// aufrufst -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container">
