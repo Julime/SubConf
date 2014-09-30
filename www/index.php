6d7c4397bc7c2e60949e81c70ea9c374cf873285
Julime <info@julime.de>
2014-09-30 21:35:47 +0200

    cleanup
    removed other persons;
    added clearfix classes;
    added some cheese

--- www/index.php
+++ www/index.php
@@ -17,120 +17,46 @@
 					                </p>
 				                </a>
 			                </div>
-			
-			                <div class="input-group">
-				                <span class="input-group-addon"><input type="checkbox"></span>
-				                <a href="#th" data-toggle="tab" class="list-group-item">
-					                <h4 class="list-group-item-heading">Thomas <span class="badge pull-right">5€</span></h4>
-					                <p class="list-group-item-text">
-						                <small>Chicken Faijta - 30cm</small>
-					                </p>
-				                </a>
-			                </div>
-			
-			                <div class="input-group">
-				                <span class="input-group-addon"><input type="checkbox"></span>
-				                <a href="#jme" data-toggle="tab" class="list-group-item">
-					                <h4 class="list-group-item-heading">Julian <span class="badge pull-right">5€</span></h4>
-					                <p class="list-group-item-text">
-						                <small>Chicken Faijta - 30cm</small>
-					                </p>
-				                </a>
-			                </div>
 		
                           </div>
                         </div>
                         <div class="col-lg-8 col-md-5 col-sm-5 tab-content border-left">
+                            
                             <div class="tab-pane" id="mn">
                                 <h3>Michael</h3>
                                 <ul class="list-group">
                                     <li class="list-group-item">
-                                        <span class="lead">Brot</span>
+                                        <span class="lead clearfix">Brot</span>
                                         <p>Honey Oat</p>
                                     </li>
                                     <li class="list-group-item">
-                                        <span class="lead">Größe</span>
+                                        <span class="lead clearfix">Größe</span>
                                         <p>30cm</p>
                                     </li>
                                     <li class="list-group-item">
-                                        <span class="lead">Fleisch</span>
+                                        <span class="lead clearfix">Fleisch</span>
                                         <p>Chicken Faijta  <span class="label label-default">Doppelt</span> <span class="label label-success">Sub des Tages</span></p>
                                     </li>
                                     <li class="list-group-item">
-                                        <span class="lead">Gemüse</span>
+                                        <span class="lead clearfix">Käse</span>
+                                        <p>Cheddar</p>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <span class="lead clearfix">Gemüse</span>
                                         <p>Salat, Gurke, Tomate, Pepperoni</p>
                                     </li>
                                     <li class="list-group-item">
-                                        <span class="lead">Sauce</span>
+                                        <span class="lead clearfix">Sauce</span>
                                         <p>Sweet Onion, Chipotle Southwest <span class="label label-default">Wenig</span></p>
                                     </li>
                                     <li class="list-group-item">
-                                        <span class="lead">Extras</span>
+                                        <span class="lead clearfix">Extras</span>
                                         <p>Salz, Pfeffer, Oregano</p>
                                     </li>
                                 </ul>
                                 <div><p><button type="button" class="btn btn-default pull-right clearfix">Bearbeiten</button></p></div>
                             </div>
-                            <div class="tab-pane" id="th">
-                                <h3>Thomas</h3>
-                                <ul class="list-group">
-                                    <li class="list-group-item">
-                                        <span class="lead">Brot</span>
-                                        <p>Sesam</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Größe</span>
-                                        <p>30cm</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Fleisch</span>
-                                        <p>Chicken Faijta  <span class="label label-default">Doppelt</span> <span class="label label-success">Sub des Tages</span></p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Gemüse</span>
-                                        <p>Salat, Gurke, Tomate, Pepperoni</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Sauce</span>
-                                        <p>Sweet Onion, Chipotle Southwest <span class="label label-default">Wenig</span></p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Extras</span>
-                                        <p>Salz, Pfeffer, Oregano</p>
-                                    </li>
-                                </ul>
-                                <div><p><button type="button" class="btn btn-default pull-right clearfix">Bearbeiten</button></p></div>
-                            </div>
-                            <div class="tab-pane" id="jme">
-                                <h3>Julian</h3>
-                                <ul class="list-group">
-                                    <li class="list-group-item">
-                                        <span class="lead">Brot</span>
-                                        <p>Italian</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Größe</span>
-                                        <p>15cm</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Fleisch</span>
-                                        <p>Chicken Faijta  <span class="label label-default">Doppelt</span> <span class="label label-success">Sub des Tages</span></p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Gemüse</span>
-                                        <p>Salat, Gurke, Tomate, Pepperoni</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Sauce</span>
-                                        <p>Sweet Onion, Chipotle Southwest <span class="label label-default">Wenig</span></p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead">Extras</span>
-                                        <p>Salz, Pfeffer, Oregano</p>
-                                    </li>
-                                </ul>
-                                <div><p><button type="button" class="btn btn-default pull-right clearfix">Bearbeiten</button></p></div>
-                            </div>
+
                         </div>
                     </div>
                 </div>
