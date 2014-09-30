a86b791567b11c3e50901e004c4aaa6ec4ac93c5
Julime <info@julime.de>
2014-09-30 21:36:44 +0200

    Edit View
    Added edit.php (just a temporary file?)

--- www/footer.php
+++ www/footer.php
@@ -1,4 +1,5 @@
         </div>
+
         <!-- jQuery (wird für Bootstrap JavaScript-Plugins benötigt) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <!-- Binde alle kompilierten Plugins zusammen ein (wie hier unten) oder such dir einzelne Dateien nach Bedarf aus -->
@@ -16,6 +17,15 @@
             $('.member-list a.active').removeClass('active');
                 $(this).addClass('active');
             });
+            
+            $('.save-btn').click(function () {
+                var btn = $(this)
+                btn.button('loading')
+                //$.ajax(...).always(function () {
+                //    btn.button('reset')
+                //});
+            });
+            
         });
         </script>
 
