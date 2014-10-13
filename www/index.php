32b3bb9902177be735176f1f65516d260dd21376
Julime <info@julime.de>
2014-10-13 22:08:58 +0200

    Read profile folder

--- www/index.php
+++ www/index.php
@@ -1,4 +1,5 @@
 <?php include 'header.php'; ?>
+<?php include 'helper/read.php'; ?>
 
             <div class="page-header">
                 <h1>SubConf</h1>
@@ -6,57 +7,79 @@
                     <div class="panel-body">
 
                         <div class="col-sm-4">
-                          <div class="list-group member-list">
-		  				
-			                <div class="input-group">
-				                <span class="input-group-addon"><input type="checkbox"></span>
-				                <a href="#mn" data-toggle="tab" class="list-group-item">
-					                <h4 class="list-group-item-heading">Michael <span class="badge pull-right">5€</span></h4>
-					                <p class="list-group-item-text">
-						                <small>Chicken Faijta - 30cm</small>
-					                </p>
-				                </a>
-			                </div>
+                            <div class="list-group member-list">
+                            
+                            <?php
+                            foreach ($profiles as $path)
+                            {
+                                $string = file_get_contents($path);
+                                $profile=json_decode($string,true);
+
+                                ?>
+
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><input type="checkbox"></span>
+                                        <a href="#<?php echo $profile["hash"]; ?>" data-toggle="tab" class="list-group-item">
+                                            <h4 class="list-group-item-heading"><?php echo $profile["name"]; ?> <span class="badge pull-right">5€</span></h4>
+                                            <p class="list-group-item-text">
+                                                <small><?php echo $profile["meat"]; ?> - <?php echo $profile["size"]; ?></small>
+                                            </p>
+                                        </a>
+                                    </div>
+                            <?php } ?>
 		
-                          </div>
+                            </div>
                         </div>
                         <div class="col-lg-8 col-md-5 col-sm-5 tab-content border-left">
                             
-                            <div class="tab-pane" id="mn">
-                                <h3>Michael</h3>
-                                <ul class="list-group">
-                                    <li class="list-group-item">
-                                        <span class="lead clearfix">Brot</span>
-                                        <p>Honey Oat</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead clearfix">Größe</span>
-                                        <p>30cm</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead clearfix">Fleisch</span>
-                                        <p>Chicken Faijta  <span class="label label-default">Doppelt</span> <span class="label label-success">Sub des Tages</span></p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead clearfix">Käse</span>
-                                        <p>Cheddar</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead clearfix">Gemüse</span>
-                                        <p>Salat, Gurke, Tomate, Pepperoni</p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead clearfix">Sauce</span>
-                                        <p>Sweet Onion, Chipotle Southwest <span class="label label-default">Wenig</span></p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="lead clearfix">Extras</span>
-                                        <p>Salz, Pfeffer, Oregano</p>
-                                    </li>
-                                </ul>
-                                <div><p><button type="button" class="btn btn-default pull-right clearfix">Bearbeiten</button></p></div>
-                            </div>
-
+                            <?php
+                            foreach ($profiles as $path)
+                            {
+                                $string = file_get_contents($path);
+                                $profile=json_decode($string, true);
+                                
+                                ?>
+                            
+                                <div class="tab-pane" id="<?php echo $profile["hash"]; ?>">
+                                    <h3>Michael</h3>
+                                    <ul class="list-group">
+                                        <li class="list-group-item">
+                                            <span class="lead clearfix">Brot</span>
+                                            <p><?php echo $profile["bread"]; ?></p>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="lead clearfix">Größe</span>
+                                            <p><?php echo $profile["size"]; ?></p>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="lead clearfix">Fleisch</span>
+                                            <p><?php echo $profile["meat"]; ?>
+                                                <?php if(isset($profile['doublemeat'])) { ?>
+                                                    <span class="label label-default">Doppelt</span>
+                                                <?php } ?>
+                                            </p>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="lead clearfix">Käse</span>
+                                            <p><?php print(implode( ', ', $profile["cheese"])); ?></p>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="lead clearfix">Gemüse</span>
+                                            <p><?php print(implode( ', ', $profile["salad"])); ?></p>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="lead clearfix">Sauce</span>
+                                            <p><?php print(implode( ', ', $profile["sauce"])); ?></p>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="lead clearfix">Extras</span>
+                                            <p><?php print(implode( ', ', $profile["extras"])); ?></p>
+                                        </li>
+                                    </ul>
+                                    <div><p><button type="button" class="btn btn-default pull-right clearfix">Bearbeiten</button></p></div>
+                                </div>
+                            <?php } ?>
+                            
                         </div>
                     </div>
                 </div>
