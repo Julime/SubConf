7776fd3c6a586237a051dd64fa848667b95258ed
Julime <info@julime.de>
2014-10-13 22:12:54 +0200

    Forgot $hash

--- www/helper/write.php
+++ www/helper/write.php
@@ -1,6 +1,7 @@
 <?php
     $hash =  hash('md5', $_POST['email']);
     $file = '../profiles/'.$hash.'.json';
+    $_POST['hash'] = $hash;
     file_put_contents($file, json_encode($_POST));
     var_dump($_POST);
 ?>
