d9b234901349b78863060ddd48eda9cdf3ebfd57
Marcel Baumann <Littellittel@gmx.de>
2015-12-22 15:55:41 +0100

    bugfix bei der preisermittlung

--- www/helper/getprice.php
+++ www/helper/getprice.php
@@ -40,7 +40,7 @@ if (!key_exists("onlycoupon", $profile)) {
 
     foreach($gutschein as $gutscheine) {
         foreach($gutscheine as $Sub) {
-            if (is_array($Sub) and key_exists("coupon",$profile) and !empty($profile["coupon"]) and in_array($Sub["name"],$profile["coupon"])) {
+            if (is_array($Sub) and key_exists("coupon",$profile) and !empty($profile["coupon"]) and in_array($Sub["name"],$profile["coupon"]) and strpos($Sub["name"],"=€")!==false) {
                 $Subprice=str_replace("=€","",$Sub["price"]);
                 $Subprice=floatval($Subprice);
                 $price=$Subprice;
@@ -53,7 +53,7 @@ if (!key_exists("onlycoupon", $profile)) {
 
 foreach($gutschein as $gutscheine) {
         foreach($gutscheine as $Sub) {
-            if (is_array($Sub) and array_key_exists("name",$Sub) and array_key_exists("count",$Sub) and array_key_exists("dates",$Sub) and array_key_exists("datee",$Sub) and array_key_exists("price",$Sub) and isset($profile["coupon"]) and is_array($profile["coupon"])) {
+            if (is_array($Sub) and array_key_exists("name",$Sub) and array_key_exists("dates",$Sub) and array_key_exists("datee",$Sub) and array_key_exists("price",$Sub) and isset($profile["coupon"]) and is_array($profile["coupon"]) and in_array($Sub["name"], $profile["coupon"])) {
         if (strpos($Sub["price"],"%")!==false){
                         $subprice=str_replace("%","",$Sub["price"]);
                         $subprice=floatval($subprice);
@@ -65,9 +65,9 @@ foreach($gutschein as $gutscheine) {
 
     foreach($gutschein as $gutscheine) {
         foreach($gutscheine as $Sub) {
-            if (is_array($Sub) and array_key_exists("name",$Sub) and array_key_exists("count",$Sub) and array_key_exists("dates",$Sub) and array_key_exists("datee",$Sub) and array_key_exists("price",$Sub) and isset($profile["coupon"]) and is_array($profile["coupon"])) {
+            if (is_array($Sub) and array_key_exists("name",$Sub) and array_key_exists("dates",$Sub) and array_key_exists("datee",$Sub) and array_key_exists("price",$Sub) and !empty($profile["coupon"]) and is_array($profile["coupon"])) {
                 if (in_array($Sub["name"], $profile["coupon"])){
-                    if (strpos($Sub["price"],"€")!==false) {
+                    if (strpos($Sub["price"],"€")!==false and strpos($Sub["price"],"=")===false) {
                         $subprice=str_replace("€","",$Sub["price"]);
                         $subprice=floatval($subprice);
                         $price=$price+$subprice;
